aed421d4901efff3c2d9345455af1c97bb3494c4
unknown <burkeamy30@gmail.com>
2019-01-20 21:46:07 -0500

    I have images! They disappear when you clkic on them :(

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,39 +1,40 @@
 $(document).ready(function() {
    var animals = ["cat", "dog", "otter", "ferret"];
    
-/*function renderButtons(animals) {
-   animals.forEach(function(animal){
+for (var i = 0; i<animals.length;i++) {
     var button = $("<button>");
     button.addClass("animalButtons");
-    button.attr("data-name", animal);
-    button.text(animal);
+    button.text(animals[i]);
     $("#animalButtons").append(button);
-    console.log(buttons);
-})*/
+};
+})
 
 $.ajax({
     url: "https://api.giphy.com/v1/gifs/search?api_key=3RAdvavxhi69zT49G8Fcr2QE3nIIdz4a&q=cat&limit=10&offset=0&rating=PG&lang=en",
     method: "GET"
     }).then(function(response) {
         console.log(response);
-        for (var i =0; i<10; i++) { 
+        for (var i =0; i<response.data.length; i++) {
         var animalDiv = $("<div class = 'animal'>");
         var stillUrl = response.data[i].images.fixed_height_still.url;
         var rating = response.data[i].rating;
         var moveUrl = response.data[i].embed_url;
         var img = $("<img>").attr("src", stillUrl);
-        var moveImg = $("<img>").attr("src", moveUrl);
             
         var ratingEl = $("<P>").text("Rating: "+ rating);
         
-        animalDiv.prepend(ratingEl, img);
-        $("#animalsView").html(animalDiv);
-
-       /*if ("src" === stillUrl) {
-           $(img).on("click", function(animate){
-            $(img).append("src", moveUrl);
-           });*/
-        }     
+        animalDiv.append(ratingEl, img);
+        $("#animalsView").before(animalDiv);
+        
+        $(img).on("click", function(){
+            if ("src" === stillUrl);
+            $(img).appendTo("src", moveUrl);
+            animalDiv.before(ratingEl, img);
+            $("#animalsView").
+             else 
+                $(img).replaceWith("src", stillUrl);
+           });
+        }    
     });
-});
+
 
