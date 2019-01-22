2213d3685629eddd2ff91c9ecb3f383879272636
unknown <burkeamy30@gmail.com>
2019-01-22 16:42:21 -0500

    not sure if this is new content

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
    var animals = ["cat", "dog", "otter", "ferret"];
-   var animal;
-   
+ 
+// this section creates the animal buttons   
 for (var i = 0; i<animals.length;i++) {
     var button = $("<button>");
     button.addClass("btn btn-primary");
     button.addClass("animalButtons");
     button.text(animals[i]);
+    button.attr("data-name", animals[i]);
     $("#animalButtons").append(button);
 }
-$("#animalButtons").click(function() { 
-    animal = animals[i];
-    console.log("click");
-    $("#animalButtons").val(animals[i]);
+
+//this code is supposed to take the user input and add a button
+$("#addAnAnimal").click(function(event) { 
+    event.preventDefault();
+    var value = $("#plusAnimal").val().trim();
+    animals.push(value);    
 });
+
+$("#animalButtons").click(function(){
+    var animal = $(this).attr("data-name");
+    console.log(this);
+    console.log(animal);
 
 $.ajax({
     url: "https://api.giphy.com/v1/gifs/search?api_key=3RAdvavxhi69zT49G8Fcr2QE3nIIdz4a&q=cat&limit=10&offset=0&rating=PG&lang=en",
@@ -24,7 +32,7 @@ $.ajax({
         var animalDiv = $("<div class = 'animal'>");
         var stillUrl = response.data[i].images.fixed_height_still.url;
         var rating = response.data[i].rating;
-        var moveUrl = response.data[i].embed_url;
+        var moveUrl = response.data[i].bitly_url;
         var img = $("<img>").attr("src", stillUrl);
             
         var ratingEl = $("<P>").text("Rating: "+ rating);
@@ -41,6 +49,7 @@ $.ajax({
              else 
                 $(img).replaceWith("src", stillUrl);*/
            });
-        }    
+        }   
+    })
     });
 })
